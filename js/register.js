644a520dd68a69588a53200428d67ec47ef901de
motiveofff <mintivetv@gmail.com>
2024-12-20 18:21:00 +0300

    Update register.js

--- js/register.js
+++ js/register.js
@@ -14,7 +14,7 @@ document.getElementById("registerForm").addEventListener("submit", async (e) => 
     }
 
     try {
-        const response = await fetch("http://localhost:3000/register", {
+        const response = await fetch("https://petshop-production-b29b.up.railway.app/register", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
