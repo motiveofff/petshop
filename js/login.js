5d9e0718e4dbd45d408d9eb81e0fcc94476d3ffb
motiveofff <mintivetv@gmail.com>
2024-12-20 18:20:50 +0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
     const password = document.getElementById("password").value.trim();
 
     try {
-        const response = await fetch("http://localhost:3000/login", {
+        const response = await fetch("https://petshop-production-b29b.up.railway.app/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
